d7dcfd898c3b7ecc423c4114be9b0c86c469cec4
MaxLyonel <leorexx100@gmail.com>
2021-09-21 19:32:39 -0400

    quinta pregunta del examen 319

---
+++ pregunta 4/Program.fs
@@ -0,0 +1,21 @@
+﻿// Pregunta N° 5 de Examen de INF - 319
+
+open System
+
+(*let sum a b = a + b*)
+let mutable c = 0
+let mul a b : int = 
+    for i in 1..b do
+        c <- c + a
+    c
+    
+// Multiplicación con sumas sucesivas de dos números
+[<EntryPoint>]
+let main argv =
+    
+    (*let c = sum 5 7
+    printfn "Suma 5+7 = %i" c*)
+    let m = mul 3 4
+    printfn "Mult 3*4 = %i" m
+    Console.ReadKey() |>ignore
+    0 // devolver un código de salida entero
